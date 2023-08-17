f933950b376505ca79a3998ece6b7e5c742a6dfa
usamabinjafarpk <usamabinjafarpk335@gmail.com>
2023-08-17 10:48:53 +0530

    second commit

--- pages/user.jsx
+++ pages/user.jsx
@@ -7,6 +7,9 @@ export default function user() {
         <div className='border border-black flex flex-col justify-center items-center w-96 h-96 rounded-lg'>
         <div className='text-base bg-indigo-900 text-white ring ring-white font-bold uppercase rounded-full w-8 h-8 flex justify-center items-center'>US</div><br></br>
         <p className='text-2xl'>Name of the user</p><br/><br/>
+        <Link href='/eventlist'>
+        <button className='border border-black w-32 rounded-lg bg-slate-300 text-black hover:bg-black hover:text-slate-300'>List of events</button>
+        </Link><br/>
         <Link href='/events'>
         <button className='border border-black w-32 rounded-lg bg-slate-300 text-black hover:bg-black hover:text-slate-300'>Add events</button>
         </Link><br/>
